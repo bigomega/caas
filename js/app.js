ed12caa0cf0e25224c292e98c29dfe1410b7a878
Bharath R <bharathraja@live.com>
2014-05-08 05:07:34 +0530

    working version of json or array in input field

--- js/app.js
+++ js/app.js
@@ -2,6 +2,41 @@ console.log('zzz...');
 $(function(){
   var draw = function(event){
     var url = $('#text_field').val();
+    var val;
+    try{ val = eval(url);} catch(e){};
+    try{ val = JSON.parse(url);} catch(e){};
+    try{ val = JSON.parse(url.replace(/([a-zA-Z0-9][^:^,^\}]*)(?=\s*:)/g, '"$1"')); } catch(e){};
+    if(val){
+      if(Object.prototype.toString.call(val) === '[object Object]'){
+        if(val[Object.keys(val)[0]].x && val[Object.keys(val)[0]].y){
+          // { blah: {x:1, y:1}, blah: {x:2, y:3 ... }}
+          val = $.map(val, function(v, i){ return v; });
+        } else {
+          //{0:12, 2:23, 3:34 ... }
+          val = $.map(val, function(v, i){ return {x: parseFloat(i) || 0, y: parseFloat(v) || 0}; });
+        }
+      } else if(Object.prototype.toString.call(val) === '[object Array]' && val[0]){
+        if(Object.prototype.toString.call(val[0]) === '[object Array]'){
+          //[[0,2], [0,34], ... ]
+          //either its one line or two. lets assume one(should write as two later)
+          val = $.map(val, function(v, i){ return {x: parseFloat(v[0]) || 0, y: parseFloat(v[1]) || 0}; });
+        } else if(typeof val[0] == 'number' || typeof val[0] =='string'){
+          //['0', 34, '51', ... ]
+          val = $.map(val, function(v, i){ return {x: i, y: parseFloat(v) || 0}; });
+        }
+      }
+    } else {
+      val = [];
+      for (var i = 0; i < 100; i++) {
+        val.push({x: i, y: Math.sin(i/10)});
+      }
+    }
+    var options = [{
+      values: val,
+      key: 'Wave 1',
+      color: '#ff7f0e',
+      area: true
+    }];
     // var val = eval(url);
     // $.ajax({
     //   url: '/fetch'
@@ -18,17 +53,17 @@ $(function(){
         ;
 
       chart.xAxis
-        .axisLabel('Time (ms)')
+        .axisLabel('X Axis')
         .tickFormat(d3.format(',r'))
         ;
 
       chart.yAxis
-        .axisLabel('Voltage (v)')
-        .tickFormat(d3.format('.02f'))
+        .axisLabel('Y Axis')
+        .tickFormat(d3.format(',r'))
         ;
 
       d3.select('#container svg')
-        .datum(sinAndCos())
+        .datum(options)
         .transition().duration(500)
         .call(chart)
         ;
